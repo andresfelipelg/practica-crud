6d964c347818cd219b7a1ce9d8b8bbdb364a63ab
andresfelipelg <andresfelipelg@gmail.com>
2021-09-28 20:06:08 -0500

    listo

--- resources/views/plantilla/plantilla.blade.php
+++ resources/views/plantilla/plantilla.blade.php
@@ -2,25 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Proyecto Crud</title>
-</head>
-<body>
-    <header>
-        @yield("cabecera")
-    </header>
-    <section>
-        @yield("contenido")
-    </section>
-    <footer>
-        @yield("cpie")
-        <p>Derechos Reservados</p>
-    </footer>
-=======
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto Crud</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="/resources/css/app.css">
@@ -45,7 +29,8 @@
         @yield('cpie')
         <p>Derechos reservados</p>
     </footer>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
->>>>>>> ea57654e3ed869ed6be90d234d677c6f43829df3
+
 </body>
 </html>
